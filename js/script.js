a941e2d77f7a50cc98b6633783fe9a4095f12220
Johann C <joso.custodio.up@phinmaed.com>
2024-02-02 23:22:53 +0800

    Added Responsiveness and tweaked elements

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ window.addEventListener("scroll",function(){
     header.classList.toggle ("sticky", window.scrollY > 100);
 
 });
+
+let menu = document.querySelector('#menu-icon');
+let navList = document.querySelector('.navlist');
+
+menu.onclick = () => {
+
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+
+}
